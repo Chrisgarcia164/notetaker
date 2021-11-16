5a5592590ed165888fd700bcd6f16e4dffe4f7a7
Christopher Garcia <chris@christophersmbp.lan>
2021-11-15 21:38:31 -0500

    added fs module

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 const path = require("path");
+
 const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+app.use(express.json());
