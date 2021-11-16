bee7073ce3ab88d093ffb0cb6cf73713fbdd8d5d
Christopher Garcia <chris@christophersmbp.lan>
2021-11-15 21:57:14 -0500

    gets data from db.json on GET /api/notes

--- server.js
+++ server.js
@@ -7,7 +7,20 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public/notes.html"));
+});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./public/index.html"));
+// });
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./db/db.json"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
+});
 app.listen(PORT, () =>
   console.log(`server listening on http://localhost:${PORT}`)
 );
